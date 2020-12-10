e66d61cf461aa0281e5daabda8ad21e864b3bf19
Franklin Martinez <fdmartinez96@gmail.com>
2020-12-10 21:17:42 +0100

    updated data to be received and to be published

--- models/reservas_models.py
+++ models/reservas_models.py
@@ -3,20 +3,6 @@ from datetime import datetime, date
 
 class ReservaIn(BaseModel):
     idReserva: int
-    fechaReserva: datetime
-    fechaLlegada: date
-    fechaSalida: date
-    habitacion: str
-    numeroPersonas: int
-    nombres: str
-    apellidos: str
-    email: str
-    telefono: int
-    tipoDocumento: str
-    numeroDocumento: int
-    pais: str
-    ciudad: str
-    direccion: str
 
 class ReservaOut(BaseModel):
     idReserva: int
